1a0f66dfb3cb65e1e0f5a14315fced35c587a1ab
TurknetMobileTeam <MobilAppDeveloper@turk.net>
2025-02-20 09:41:00 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,28 @@
-// swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SwiftyPing",
+    platforms: [
+        .iOS(.v11),      // veya projenize uygun platformları ekleyin
+        .macOS(.v10_15)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyPing",
-            targets: ["SwiftyPing"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["SwiftyPing"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyPing",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"  // kaynak dosyalarınızın bulunduğu dizini belirtin
+        ),
         .testTarget(
             name: "SwiftyPingTests",
-            dependencies: ["SwiftyPing"]),
+            dependencies: ["SwiftyPing"],
+            path: "Tests"   // test dosyalarınızın bulunduğu dizin
+        ),
     ]
 )
